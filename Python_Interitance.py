eeaf4ad6b704574d2386c791f4f02ddf2006583b
Raghu <102865697+raghu1730@users.noreply.github.com>
2023-04-27 20:19:20 -0400

    Add files via upload

--- Python_Interitance.py
+++ Python_Interitance.py
@@ -13,7 +13,7 @@ class SoftwareEngineer(Employee):
         super().__init__(name, age, salary)
         self.level = level
     def work(self):
-        print(f"{self.name} is working in Goodle")
+        print(f"{self.name} is working in Goodle and he is {self.level} and salary is {self.salary}")
     def debug(self):
         print(f"{self.name} is debugging")
 class Designer(Employee):   # subclass or child class
@@ -21,13 +21,32 @@ class Designer(Employee):   # subclass or child class
         print(f"{self.name} is working in amazon")
     def draw(self):
         print(f"{self.name} is drawing")
+    def sal_cal(self, age):
+        if age < 25:
+            return 5000
+        if age < 35:
+            return 7000
+        else:
+            return 8000
+    def eval(self):
+        #self.salary
+        amount = float(input(f"Enter how many Hr {self.name} worked:"))
+        self.salary *=amount
+        print(f"The total salary is:{self.salary}")
 #instance
-se= SoftwareEngineer("John",25, 5000, "Junior")
+se= SoftwareEngineer("John",22, 5000, "Junior")
 print(se.name, se.age, se.salary)
 #se2 = SoftwareEngineer("David", 28, 4000, "Senior")
+
 se.work()
+se.debug()
 #se2.work()
 d=Designer("David", 28, 4000)
 print(d.name, d.age, d.salary)
 d.work()
 d.draw()
+print(d.sal_cal(30))
+d.eval()
+
+
+
